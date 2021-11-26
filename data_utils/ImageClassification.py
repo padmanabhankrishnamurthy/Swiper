421c822d85b4bfe19ce22c5f2a4cd46b9a86628e
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2021-11-26 13:43:57 -0500

    added dataloader tests

--- data_utils/ImageClassification.py
+++ data_utils/ImageClassification.py
@@ -26,10 +26,11 @@ class ImageClassificationDataset(Dataset):
     def __getitem__(self, idx):
         image = os.path.join(self.images_dir, self.images[idx])
         image_name = self.images[idx]
-        image = np.asarray(Image.open(image))/255
-        image = image.astype(np.uint8)
+        # image = np.asarray(Image.open(image))/255
+        image = np.asarray(Image.open(image))
+        # image = image.astype(np.uint8)
 
-        transforms = T.Compose([T.ToPILImage(), T.Resize((256,256)), T.ToTensor(),
+        transforms = T.Compose([T.ToPILImage(), T.ToTensor(),
                                 T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
         image = transforms(image)
@@ -43,8 +44,23 @@ class ImageClassificationDataset(Dataset):
 if __name__ == '__main__':
     image_dir = '../data'
     dataset = ImageClassificationDataset(image_dir)
+    print(dataset.num_classes)
     for data in dataset:
-        image, label = data
-        print(image.shape, torch.argmax(label).item(), dataset.unique_words[torch.argmax(label).item()])
+        image, label_one_hot = data
+
+        label_index = torch.argmax(label_one_hot).item()
+        label = dataset.unique_words[label_index]
+        print(image.shape, label_index, label)
+
+        inv_normalize = T.Normalize(mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.255],std=[1 / 0.229, 1 / 0.224, 1 / 0.255])
+        image = inv_normalize(image)
+        image = torch.permute(image, [1,2,0])
+        image = image.numpy()*255
+        image = image.astype(np.uint8)
+        plt.imshow(image)
+        plt.title(label)
+        plt.show()
+
+
 
 
