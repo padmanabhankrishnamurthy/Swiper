66266f3fd2bccfa5b3552b468c83ffa97560fe60
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2021-11-26 13:43:21 -0500

    check what words are stored before data collection

--- data_collection.py
+++ data_collection.py
@@ -3,7 +3,15 @@ import os
 import string
 import enchant
 
-def get_unique_words(path):
+def get_stored_words(save_path):
+    stored = set()
+    for file in os.listdir(save_path):
+        if 'jpg' in file and file[:file.find('_')] not in stored:
+            stored.add(file[:file.find('_')])
+    return stored
+
+def get_unique_words(path, save_path):
+    stored_words = get_stored_words(save_path)
 
     d = enchant.Dict("en_US")
 
@@ -12,16 +20,23 @@ def get_unique_words(path):
     for line in file:
         line = line.translate(str.maketrans('', '', string.punctuation))
         for word in line.split():
-            if word not in words and d.check(word): # we don't want stuff like wooooohooooooo in our lexicon
+            if word not in stored_words and word not in words and d.check(word): # we don't want stuff like wooooohooooooo in our lexicon
                 words.add(word)
     return list(words)
 
-
-if __name__ == '__main__':
+def main():
     lyrics_path = 'lyrics.txt'
-    words = get_unique_words(lyrics_path)
-    print(words, len(words))
-
     save_path = 'data'
 
+    words = get_unique_words(lyrics_path, save_path)
+    print(words, len(words))
+
     detect_hands(words, save_path)
+
+if __name__ == '__main__':
+    main()
+    # stored_words = get_stored_words('data')
+    # unique_words = get_unique_words('lyrics.txt', 'data')
+    # print(len(stored_words), stored_words)
+    # print(len(unique_words), unique_words)
+    # print([word for word in stored_words if word in unique_words])
