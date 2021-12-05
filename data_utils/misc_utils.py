49aea819156337cd2d5d99b87accc4bbb997f23d
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2021-12-05 16:32:47 -0500

    added label_tensor_to_char and clean_decoder_output utils

--- data_utils/misc_utils.py
+++ data_utils/misc_utils.py
@@ -23,3 +23,24 @@ def get_start_sequence_tensor(train_dataset):
     start_token = torch.stack([F.one_hot(char_tensor, len(train_dataset.index_to_char_mapping)) for char_tensor in start_token])
     start_token = start_token.type(torch.FloatTensor)
     return start_token
+
+def get_index_to_char_mapping():
+    index_to_char_mapping = {0: '<start>'}
+    index_to_char_mapping.update({i + 1: chr(c) for i, c in enumerate(range(ord('a'), ord('z') + 1))})
+    index_to_char_mapping[27] = '<end>'
+    index_to_char_mapping[28] = '<pad>'
+    return index_to_char_mapping
+
+def label_tensor_to_char(char_sequence):
+    index_to_char_mapping = get_index_to_char_mapping()
+    label = [index_to_char_mapping[torch.argmax(tensor).item()] for tensor in char_sequence]
+    return label
+
+def clean_decoder_output(decoder_output):
+    if '<end>' in decoder_output:
+        decoder_output = decoder_output[decoder_output.index('<start>') + 1:decoder_output.index('<end>')]
+    else:
+        decoder_output = decoder_output[decoder_output.index('<start>') + 1:decoder_output.index('<pad>')]
+    decoder_output = ''.join(decoder_output)
+
+    return decoder_output
